0f9cb5d1e109cf5d8265ba132a5bc17522d000a5
AlexCo67 <alexandrecolombo@aol.com>
2021-09-11 16:11:48 +0200

    FRONTEND: fix script.js

--- FRONTEND/script.js
+++ FRONTEND/script.js
@@ -1,12 +1,42 @@
+function StringContainsLowerCase(stringToTest){
+	return (stringToTest !== stringToTest.toLowerCase());
+}
+
+function StringContainsUpperCase(stringToTest){
+	return (stringToTest !== stringToTest.toUpperCase());
+}
+
+function StringContainsNumbers(stringToTest){
+	let numberRegex = new RegExp(".*[0-9].*");
+return (numberRegex.test(stringToTest));
+}
+
 $("form").submit(function(e){
+
 	let passwordValue = $("#pswd").val();
-	let passwordRegex = new RegExp('[0-9a-zA-Z]{10,20}');  
-	if (passwordRegex.test(passwordValue)) {
-            alert('Le mot de passe est conforme');
-        }
-		else{
-			alert("Error");
-		e.preventDefault();
-		return false;
+	let passwordRegex = new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{10,20}$');
+
+	if (passwordRegex.test(passwordValue)==true) {
+		alert('Le mot de passe est conforme');
+	}
+	else{
+			if(passwordValue.length<10){
+				alert('Le mot de passe est trop court');
+				}
+			if(passwordValue.length>20){
+				alert('Le mot de passe est trop long');
+			}
+			if(StringContainsLowerCase(passwordValue)==false){
+				alert('Le mot de passe doit contenir au moins une minuscule');
+			}
+
+			if(StringContainsUpperCase(passwordValue)==false){
+				alert('Le mot de passe doit contenir au moins une majuscule');
+			}
+			if(StringContainsNumbers(passwordValue)==false){
+				alert('Le mot de passe doit contenir au moins un chiffre');
+			}
+			e.preventDefault();
+			return false;
 		}
 });
